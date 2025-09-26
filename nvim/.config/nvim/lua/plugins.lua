483518b355dfd9f719b56ad709f78fecaa56ed5c
Ryan Dielhenn <dielhennr@gmail.com>
2025-09-26 04:46:41 -0700

    switch-nvim-plugin-mgr

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,18 +1,50 @@
-vim.cmd([[
-call plug#begin('~/.vim/plugged')
-  Plug 'sainnhe/everforest'
-  Plug 'ojroques/nvim-osc52'
-  Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }
-  Plug 'junegunn/fzf.vim'
-  Plug 'neoclide/coc.nvim', {'branch': 'release'}
-  Plug 'vim-airline/vim-airline'
-  Plug 'vim-airline/vim-airline-themes'
-  Plug 'edkolev/tmuxline.vim'
-  Plug 'iamcco/markdown-preview.nvim', { 'do': 'cd app && npx --yes yarn install' }
-  Plug 'tpope/vim-fugitive'
-  Plug 'airblade/vim-gitgutter'
-  Plug 'skywind3000/asyncrun.vim'
-  Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
-  Plug 'tpope/vim-vinegar'
-call plug#end()
-]])
+-- ~/.config/nvim/lua/plugins.lua
+
+-- Bootstrap lazy.nvim if not installed
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Plugin setup
+require("lazy").setup({
+  { "sainnhe/everforest" },
+  { "ojroques/nvim-osc52" },
+  { "junegunn/fzf", build = "./install --bin" }
+  { "junegunn/fzf.vim" },
+  { "neoclide/coc.nvim", branch = "release" },
+  { "vim-airline/vim-airline" },
+  { "vim-airline/vim-airline-themes" },
+  { "edkolev/tmuxline.vim" },
+  {
+    "iamcco/markdown-preview.nvim",
+    build = "cd app && npx --yes yarn install",
+    ft = { "markdown" }, -- lazy-load only for markdown files
+  },
+  { "tpope/vim-fugitive" },
+  { "airblade/vim-gitgutter" },
+  { "skywind3000/asyncrun.vim" },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+  },
+  { "tpope/vim-vinegar" },
+}, {
+  ui = {
+    border = "rounded", -- nice rounded borders in the :Lazy UI
+  },
+  install = {
+    missing = true, -- auto-install any missing plugins on startup
+  },
+  checker = {
+    enabled = true,  -- auto-check for plugin updates
+    notify = false,  -- don't spam with notifications
+  },
+})
