61f5b03c77ebad5df3a3d5ae275aad332d52fad8
Ryan Dielhenn <dielhennr@gmail.com>
2026-03-13 12:08:49 -0700

    add java doc inline resolvesupport

--- nvim/.config/nvim/after/ftplugin/java.lua
+++ nvim/.config/nvim/after/ftplugin/java.lua
@@ -1,4 +1,10 @@
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.resolveSupport = {
+  properties = { "documentation", "detail", "additionalTextEdits" }
+}
+
 require("jdtls").start_or_attach({
   cmd = { "jdtls" },
   root_dir = vim.fs.dirname(vim.fs.find({ ".git", "gradlew", "mvnw" }, { upward = true })[1]),
+  capabilities = capabilities,
 })
