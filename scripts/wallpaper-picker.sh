ddeb2f11fc40d931f00daf6ba45cc638cdb7ea3d
Ryan Dielhenn <dielhennr@gmail.com>
2026-06-28 05:41:34 -0700

    nits

--- scripts/wallpaper-picker.sh
+++ scripts/wallpaper-picker.sh
@@ -5,7 +5,8 @@ base="$1"
 selection=$(cd "$base" && find . -path ./.git -prune -o -type f \
     \( -iname '*.jpg' -o -iname '*.jpeg' -o -iname '*.png' -o -iname '*.webp' \) -print \
     | sed 's|^\./||' | sort \
-    | dmenu -c -a "$2" -i -l 10 -fn "$3" -nb "$4" -nf "$5" -sb "$6" -sf "$7")
+    | dmenu -c -a "$2" -i -l 40 -fn "$3" -nb "$4" -nf "$5" -sb "$6" -sf "$7")
 
 [ -n "$selection" ] || exit 0
+ln -sf "$base/$selection" "$base/pick.jpg"
 feh --bg-scale "$base/$selection"
