5c4f962aac03ca91a93e09f096da75cb8ea7d2f3
Ryan Dielhenn <dielhennr@gmail.com>
2026-06-28 04:38:42 -0700

    only show battery status if exisats

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
 readonly battery=/sys/class/power_supply/BAT0
-read -r capacity < "$battery/capacity"
-read -r status < "$battery/status"
 
-if [[ $status == Charging ]]; then
-  icon='󱐋'
-else
-  icons=(󰂎 󰁺 󰁻 󰁼 󰁽 󰁾 󰁿 󰂀 󰂁 󰂂 󰁹)
-  icon=${icons[capacity / 10]}
+if [ -f "$battery" ]; then
+  read -r capacity < "$battery/capacity"
+  read -r status < "$battery/status"
+
+  if [[ $status == Charging ]]; then
+    icon='󱐋'
+  else
+    icons=(󰂎 󰁺 󰁻 󰁼 󰁽 󰁾 󰁿 󰂀 󰂁 󰂂 󰁹)
+    icon=${icons[capacity / 10]}
+  fi
+
+  printf '%s %s%%\n' "$icon" "$capacity"
 fi
-
-printf '%s %s%%\n' "$icon" "$capacity"
